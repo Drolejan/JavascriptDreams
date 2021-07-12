b5a63fd6867bc5eaff8d62dd60439f6cfe95faad
Alejandro Medina <drolejan@gmail.com>
2021-07-12 13:23:11 -0500

    Taller 1 Terminado

--- figuras.js
+++ figuras.js
@@ -9,11 +9,22 @@ function areaCuadrado(lado){
 }
 
 function perimetroTriangulo(lado1,lado2,base){
-    return lado1+lado2+base;
+    return (Number(lado1) + Number(lado2) + Number(base));
 }
 
 function areaTriangulo(base,altura){
     return (base*altura)/2;
+}
+
+function areaIsosceles(lado1,lado2,base){
+    if(lado1===lado2){
+        const cateto=base/2;
+        const altura=Math.sqrt((lado1**2)-(cateto**2));
+        return(base*altura)/2;
+    }
+    else{
+        alert("Esto no es un triangulo isosceles");
+    }
 }
 
 function diametroCirculo(radio){
@@ -43,6 +54,51 @@ function calcularPerimetroCuadrado(){
 function calcularAreaCuadrado() {
     const input = document.getElementById("InputCuadrado");
     const value = input.value;
-    const perimetro = areaCuadrado(value);
+    const area = areaCuadrado(value);
+    alert(area);
+}
+
+function calcularPerimetroTriangulo(){
+    const input1 = document.getElementById("InputTriangulo1");
+    const lado1 = input1.value;
+    const input2 = document.getElementById("InputTriangulo2");
+    const lado2 = input2.value;
+    const input3 = document.getElementById("InputTriangulo3");
+    const base = input3.value;
+    const perimetro = perimetroTriangulo(lado1,lado2,base);
     alert(perimetro);
+}
+   
+function calcularAreaTriangulo() {
+    const input3 = document.getElementById("InputTriangulo3");
+    const base = input3.value;
+    const input4 = document.getElementById("InputTriangulo4");
+    const altura = input4.value;
+       const area = areaTriangulo(base,altura);
+       alert(area);
+}
+
+function calcularPerimetroCirculo(){
+    const input = document.getElementById("InputCirculo");
+    const radio = input.value;
+    const perimetro = perimetroCirculo(radio);
+    alert(perimetro);
+}
+   
+function calcularAreaCirculo() {
+    const input = document.getElementById("InputCirculo");
+    const radio = input.value;
+       const area = areaCirculo(radio);
+       alert(area);
+}
+
+function calcularAreaIsoceles() {
+    const input3 = document.getElementById("InputLado1");
+    const lado1 = input3.value;
+    const input4 = document.getElementById("InputLado2");
+    const lado2 = input4.value;
+    const input5 = document.getElementById("InputBase");
+    const base = input5.value;
+       const area = areaIsosceles(lado1,lado2,base);
+       alert(area);
 }
